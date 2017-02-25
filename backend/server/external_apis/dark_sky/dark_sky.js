916262cbeb82fcc6d244a6e94ef60d8b7efb0d70
cchockal <cchockal@iastate.edu>
2017-02-24 22:58:33 -0600

    Added data needed from API calls and API connection information

--- backend/server/external_apis/dark_sky/dark_sky.js
+++ backend/server/external_apis/dark_sky/dark_sky.js
@@ -1,1 +1,2 @@
-//TODO
+var DarkSkyPI = require('darksky');
+var apiKey = '7fac34997f7ef8f5d3867514a57b71b0';
--- backend/server/external_apis/google_maps/google_maps.js
+++ backend/server/external_apis/google_maps/google_maps.js
@@ -1,1 +1,12 @@
-//TODO
+var GoogleMapsAPI = require('googlemaps');
+
+var publicConfig = {
+  key: 'AIzaSyDEaX0V5IP1RXC0OWAjeGIFM5ZyoVp1p74',
+  stagger_time:       1000, // for elevationPath
+  encode_polylines:   false,
+  secure:             true // use https
+};
+
+var gmAPI = new GoogleMapsAPI(publicConfig);
+
+module.exports = gmAPI;
