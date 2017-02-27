80976bdf31731217d8a847f4273280294a3c6b06
jp51371 <jp51371@iastate.edu>
2017-02-27 01:17:26 -0600

    Started Working on Google_Maps API

--- backend/server/external_apis/google_maps/google_maps.js
+++ backend/server/external_apis/google_maps/google_maps.js
@@ -1,4 +1,4 @@
-var GoogleMapsAPI = require('googlemaps');
+/*var GoogleMapsAPI = require('googlemaps');
 
 var publicConfig = {
   key: 'AIzaSyDEaX0V5IP1RXC0OWAjeGIFM5ZyoVp1p74',
@@ -10,3 +10,62 @@ var publicConfig = {
 var gmAPI = new GoogleMapsAPI(publicConfig);
 
 module.exports = gmAPI;
+*/
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyDEaX0V5IP1RXC0OWAjeGIFM5ZyoVp1p74'
+});
+//https://googlemaps.github.io/google-maps-services-js/docs/GoogleMapsClient.html
+module.exports = {
+  getDirections : function(start, end, callback){
+    startLatLng = [start.lat, start.lng];
+    endLatLng = [end.lat, end.lng];
+    var params = {
+      origin : startLatLng,
+      destination : endLatLng
+    }
+    googleMapsClient.directions(params, function(err, response){
+      if(err){
+        callback(err, null);
+      }
+      else{
+        var routesToReturn = [];
+        var routes = response.routes;
+        for(var index = 0; index < routes.length; index++){
+          routesToReturn.push(getARoute(routes[index]));
+        }
+        callback(null, routesToReturn);
+      }
+    });
+  },
+
+  getLocationsAlongPath : function(start, end){
+    //TODO
+  },
+
+  getTownName : function(location){
+    //TODO
+  }
+}
+
+var getARoute = function(route){
+  var paths = [];
+  var legs = route.legs;
+  var numPath = 1;
+  for(var index = 0; index < legs.length; index++){
+    var newPaths = getPaths(legs[index], numPath);
+    numPath += newPaths.length;
+    paths.append(newPaths);
+  }
+}
+
+var getPaths = function(leg, numPath){
+  var Path = require('./paths');
+  var paths = [];
+  var steps = leg.steps;
+  for(var index = 0; index < steps.length; index++){
+    var path = new Path(steps[index], numPath);
+    paths.push(path);
+    numPath++;
+  }
+  return paths;
+}
---
+++ backend/server/external_apis/google_maps/paths.js
@@ -0,0 +1,17 @@
+function Path(step, numPath){
+  this.distance = metersToMiles(step.distance.value);
+  this.polyline = step.polyline.points;
+  var startLat = step.start_location.lat;
+  var startLng = step.start_location.lng;
+  this.start = [startLat, startLng];
+  var endLat = step.end_location.lat;
+  var endLng = step.end_location.lng;
+  this.end = [endLat, endLng];
+  this.numPath = numPath;
+}
+
+var metersToMiles = function(meters){
+  return meters * 0.000612371;
+}
+
+module.exports = Path;
